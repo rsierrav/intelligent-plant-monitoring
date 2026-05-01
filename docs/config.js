532ce3241b712bafffb73eb02586fcd0b1c47d1d
Rosa Sierra Villanueva <rosagloriasv27@gmail.com>
2026-05-01 01:38:50 -0400

    Remove example config file and add actual configuration for Supabase and API URL

---
+++ docs/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SUPABASE_URL: "https://flsaxzytjuvfcdxxkjht.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc2F4enl0anV2ZmNkeHhramh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxNjExNTcsImV4cCI6MjA4NzczNzE1N30.nSIPF_aXb47fAxEP-6jyjI6fVsUkldx-TvJtvJb5Shg",
+  API_URL: "https://plant-dashboard-gsiq.onrender.com"
+};
